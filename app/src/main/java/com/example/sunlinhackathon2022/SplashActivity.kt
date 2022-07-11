dbec7cad5ec3583a52dced2251ee1271d1f8217f
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-11 15:11:13 +0900

    스플래쉬 엑티비티

--- app/src/main/java/com/example/sunlinhackathon2022/SplashActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.example.sunlinhackathon2022
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            val intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
     }
 }
