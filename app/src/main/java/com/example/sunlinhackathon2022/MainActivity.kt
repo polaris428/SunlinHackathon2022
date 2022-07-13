12fce61a422c3aa17504f7c33bb1f95ee09967f3
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-13 17:16:55 +0900

    바텀네비게이션바

--- app/src/main/java/com/example/sunlinhackathon2022/MainActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.sunlinhackathon2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.example.sunlinhackathon2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction().add(R.id.frameLayout, BlankFragment()).commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            replaceFragment(
+                when (it.itemId) {
+                    R.id.item_fragment1 -> BlankFragment()
+                    R.id.item_fragment2 -> BlankFragment2()
+                    R.id.item_fragment3 -> BlankFragment3()
+                    R.id.item_fragment4 -> BlankFragment4()
+                    else -> BlankFragment()
+                }
+            )
+            true
+        }
+
+
+    }
+    private fun replaceFragment(fragment: Fragment): Boolean {
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit()
+        return true
     }
 }
