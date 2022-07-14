549cf8e86456c471a444b93acca3a64cc7b4a60a
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-14 15:04:42 +0900

    입포트 문제 해결

--- app/src/main/java/com/example/sunlinhackathon2022/MainActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.sunlinhackathon2022.databinding.ActivityMainBinding
+import com.example.sunlinhackathon2022.fragment.BlankFragment
+import com.example.sunlinhackathon2022.fragment.BlankFragment2
+import com.example.sunlinhackathon2022.fragment.BlankFragment3
+import com.example.sunlinhackathon2022.fragment.BlankFragment4
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
