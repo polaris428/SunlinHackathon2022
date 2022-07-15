1512cbc5c41bfdb028577669a040daf75c2c7d68
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-15 12:37:08 +0900

    QR코드 스캔

--- app/src/main/java/com/example/sunlinhackathon2022/MainActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.example.sunlinhackathon2022
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.sunlinhackathon2022.databinding.ActivityMainBinding
 
 import com.example.sunlinhackathon2022.fragment.*
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
+    lateinit var integrator: IntentIntegrator
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.qrButton.setOnClickListener {
+            integratoInit()
+        }
+
         supportFragmentManager.beginTransaction().add(R.id.frameLayout, HomeFragment()).commit()
         binding.bottomNavigationView.background = null
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
@@ -33,8 +41,47 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
     private fun replaceFragment(fragment: Fragment): Boolean {
         supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit()
         return true
+    }
+    fun integratoInit(){
+        integrator=IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE) // 여러가지 바코드중에 특정 바코드 설정 가능
+        integrator.setPrompt("QR 코드를 스캔하여 주세요:)") // 스캔할 때 하단의 문구
+        integrator.setCameraId(0) // 0은 후면 카메라, 1은 전면 카메라
+        integrator.setBeepEnabled(true) // 바코드를 인식했을 때 삑 소리유무
+        integrator.setBarcodeImageEnabled(false) // 스캔 했을 때 스캔한 이미지 사용여부
+        integrator.initiateScan() // 스캔
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        // QR 코드를 찍은 결과를 변수에 담는다.
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        Log.d("TTT", "QR 코드 체크")
+
+        //결과가 있으면
+        if (result != null) {
+            // 컨텐츠가 없으면
+            if (result.contents == null) {
+                //토스트를 띄운다.
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            }
+            // 컨텐츠가 있으면
+            else {
+                //토스트를 띄운다.
+                Toast.makeText(this, "scanned" + result.contents, Toast.LENGTH_LONG).show()
+                Log.d("TTT", "QR 코드 URL:${result.contents}")
+
+                //웹뷰 설정
+                //웹뷰를 띄운다.
+
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
