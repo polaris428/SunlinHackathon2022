4d4530e6998f5eec060a5b53f4ccd06d1bd9c7f8
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2022-07-15 17:17:44 +0900

    상품 상세

--- app/src/main/java/com/example/sunlinhackathon2022/MyPageActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/MyPageActivity.kt
@@ -2,10 +2,20 @@ package com.example.sunlinhackathon2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sunlinhackathon2022.databinding.ActivityMyPageBinding
 
 class MyPageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMyPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_page)
+        binding = ActivityMyPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.back.setOnClickListener{
+            finish()
+        }
+
+
+
     }
 }
