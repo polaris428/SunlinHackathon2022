43c82071e460c1dbd5bb39453613d87858344ec5
Hyunju Yang <iam@2tle.io>
2022-07-15 18:42:28 +0900

    update: build.gradle (room) and AnimalDetailActivity

--- app/src/main/java/com/example/sunlinhackathon2022/AnimalDetailActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/AnimalDetailActivity.kt
@@ -2,10 +2,17 @@ package com.example.sunlinhackathon2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sunlinhackathon2022.databinding.ActivityAnimalDetailBinding
+import com.example.sunlinhackathon2022.databinding.ActivityMyPageBinding
 
 class AnimalDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAnimalDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_animal_detail)
+        binding = ActivityAnimalDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val animalCode = intent.getStringExtra("animalCode")
+
     }
 }
