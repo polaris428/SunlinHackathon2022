fce8914d9316408a7f361f2fa30fe680454ed7b6
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-15 12:12:30 +0900

    프래그먼트 디자인 변경 및 탭바 제거

--- app/src/main/java/com/example/sunlinhackathon2022/MainActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/MainActivity.kt
@@ -5,10 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.sunlinhackathon2022.databinding.ActivityMainBinding
-import com.example.sunlinhackathon2022.fragment.BlankFragment
-import com.example.sunlinhackathon2022.fragment.BlankFragment2
-import com.example.sunlinhackathon2022.fragment.BlankFragment3
-import com.example.sunlinhackathon2022.fragment.BlankFragment4
+
+import com.example.sunlinhackathon2022.fragment.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,17 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportFragmentManager.beginTransaction().add(R.id.frameLayout, BlankFragment()).commit()
-
+        supportFragmentManager.beginTransaction().add(R.id.frameLayout, HomeFragment()).commit()
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
         binding.bottomNavigationView.setOnItemSelectedListener {
 
             replaceFragment(
                 when (it.itemId) {
-                    R.id.item_fragment1 -> BlankFragment()
-                    R.id.item_fragment2 -> BlankFragment2()
-                    R.id.item_fragment3 -> BlankFragment3()
-                    R.id.item_fragment4 -> BlankFragment4()
-                    else -> BlankFragment()
+                    R.id.item_fragment1 -> HomeFragment()
+                    R.id.item_fragment2 -> DictionaryFragment()
+                    R.id.item_fragment3 -> CommunityFragment()
+                    R.id.item_fragment4 -> ShopFragment()
+                    else -> HomeFragment()
                 }
             )
             true
--- app/src/main/java/com/example/sunlinhackathon2022/fragment/DictionaryFragment.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/fragment/DictionaryFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BlankFragment4.newInstance] factory method to
+ * Use the [DictionaryFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BlankFragment4 : Fragment() {
+class DictionaryFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class BlankFragment4 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank4, container, false)
+        return inflater.inflate(R.layout.fragment_dictionary, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class BlankFragment4 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment4.
+         * @return A new instance of fragment DictionaryFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BlankFragment4().apply {
+            DictionaryFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
