aaf92cf5563aeb1eb30a5b3a70a3025998164daa
Hyunju Yang <iam@2tle.io>
2022-07-16 02:54:55 +0900

    update: illurbook api update

---
+++ app/src/main/java/com/example/sunlinhackathon2022/fragment/illustratedbook/DictData.kt
@@ -0,0 +1,3 @@
+package com.example.sunlinhackathon2022.fragment.illustratedbook
+
+data class DictData(var dict: List<Int>)
--- app/src/main/java/com/example/sunlinhackathon2022/fragment/illustratedbook/DictionaryFragment.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/fragment/illustratedbook/DictionaryFragment.kt
@@ -1,6 +1,7 @@
 package com.example.sunlinhackathon2022.fragment.illustratedbook
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +9,14 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import com.example.sunlinhackathon2022.R
+import com.example.sunlinhackathon2022.RetrofitClass
 import com.example.sunlinhackathon2022.databinding.FragmentCommunityBinding
 import com.example.sunlinhackathon2022.databinding.FragmentDictionaryBinding
 import com.example.sunlinhackathon2022.dict.AnimalDTO
 import com.example.sunlinhackathon2022.dict.AnimalDatabase
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +40,21 @@ class DictionaryFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
 
+
+
+
+        //illustratedbookAdapter.listData=  리스트 삽입
+        //illustratedbookAdapter.notifyDataSetChanged()
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentDictionaryBinding.inflate(inflater,container,false)
+
         val animalDataList: List<List<String>> = listOf( // 0 이름 1 ar 2 이미지 3 디테일
             listOf("대왕판다","GiantPanda/model.glb","https://cdn.discordapp.com/attachments/997399980765618177/997400102853414972/panda.jpg",""),
             listOf("너구리","Raccoon/model.glb","https://cdn.discordapp.com/attachments/997399980765618177/997400103927160873/raccoon.jpg",""),
@@ -46,6 +66,10 @@ class DictionaryFragment : Fragment() {
             listOf("호랑이","Tiger/model.glb","https://cdn.discordapp.com/attachments/997399980765618177/997400103532904449/tiger.jpg","")
         )
 
+        val sharedPreferences = binding.root.context.getSharedPreferences("account", 0)
+
+        val token =sharedPreferences.getString("token","").toString()
+        Log.d("token",token)
 
 
         /* RecyclerView 코드 작성해야 하는 부분
@@ -55,17 +79,28 @@ class DictionaryFragment : Fragment() {
         var illustratedbookAdapter=illustratedbookAdapter()
         binding.recyclerView.layoutManager= LinearLayoutManager(binding.root.context)
         binding.recyclerView.adapter = illustratedbookAdapter
-        //illustratedbookAdapter.listData=  리스트 삽입
-        illustratedbookAdapter.notifyDataSetChanged()
+
+        val getCall = RetrofitClass.getApiService().getDictList(token)
+        getCall.enqueue(object: Callback<DictData> {
+            override fun onResponse(call: Call<DictData>, response: Response<DictData>) {
+                if(response.isSuccessful) {
+                    val idList = ArrayList<illustratedBookData>()
+                    for(idx in response.body()!!.dict){
+                        idList.add(illustratedBookData(animalDataList[idx-1][2], animalDataList[idx-1][0]))
+                    }
+                    Log.d("dogam",idList.toString())
+                    illustratedbookAdapter.listData=idList
+                    illustratedbookAdapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onFailure(call: Call<DictData>, t: Throwable) {
+                Log.d("DictFrag","error")
+            }
+
+        })
 
 
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding=FragmentDictionaryBinding.inflate(inflater,container,false)
         return binding.root
     }
 
