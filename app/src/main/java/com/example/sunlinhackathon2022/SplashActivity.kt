5f2e7370a61238ea8c89dcf46fefb1ba4b9c1981
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-15 13:50:38 +0900

    스플래쉬화면 랜덤

--- app/src/main/java/com/example/sunlinhackathon2022/SplashActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/SplashActivity.kt
@@ -4,11 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sunlinhackathon2022.databinding.ActivityMainBinding
+import com.example.sunlinhackathon2022.databinding.ActivitySplashBinding
+import com.google.zxing.integration.android.IntentIntegrator
+import java.util.*
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val random = Random()
+
+        if(random.nextInt(1)==1){
+            binding.constraintLayout.setBackgroundResource(R.drawable.oceanback)
+        }else{
+
+        }
         val handler = Handler()
         handler.postDelayed(Runnable {
             val intent=Intent(this,WelcomeActivity::class.java)
