ab6d34658c1067878c61c8e61dca5ada4459d826
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-16 04:17:20 +0900

    도감 리사이클러뷰

--- app/src/main/java/com/example/sunlinhackathon2022/fragment/illustratedbook/illustratedbookAdapter.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/fragment/illustratedbook/illustratedbookAdapter.kt
@@ -3,19 +3,21 @@ package com.example.sunlinhackathon2022.fragment.illustratedbook
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.sunlinhackathon2022.databinding.ItemIllustratedbookBinding
+import com.example.sunlinhackathon2022.databinding.ItemDictionaryBinding
+
 
 
 class illustratedbookAdapter : RecyclerView.Adapter<illustratedbookAdapter.Holder>(){
     var listData = mutableListOf<illustratedBookData>()
-    class Holder(val binding: ItemIllustratedbookBinding): RecyclerView.ViewHolder(binding.root) {
+    class Holder(val binding: ItemDictionaryBinding): RecyclerView.ViewHolder(binding.root) {
         fun setData(illustratedBookData: illustratedBookData){
-
+            binding.root.context
+            binding.animalName.text=illustratedBookData.name
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val binding = ItemIllustratedbookBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemDictionaryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return illustratedbookAdapter.Holder(binding)
     }
