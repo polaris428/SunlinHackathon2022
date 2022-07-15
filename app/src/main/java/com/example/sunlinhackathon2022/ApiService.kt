3539c659ba425fa7719c76a8d0f7184e9acf98ac
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-15 20:33:38 +0900

    바코드 생성 버그 해결

--- app/src/main/java/com/example/sunlinhackathon2022/ApiService.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/ApiService.kt
@@ -5,6 +5,7 @@ import com.example.sunlinhackathon2022.account.NewUserData
 import com.example.sunlinhackathon2022.account.SignInData
 import com.example.sunlinhackathon2022.account.SignUpData
 import com.example.sunlinhackathon2022.fragment.shop.ShopData
+import com.example.sunlinhackathon2022.fragment.shop.purchase.Barcode
 import com.example.sunlinhackathon2022.fragment.shop.purchase.Buy
 import retrofit2.Call
 import retrofit2.http.Body
@@ -20,7 +21,7 @@ interface ApiService {
     @GET("shop")
     fun getShop(): Call<ShopData>
     @POST("barcode")
-    fun setBarcode(@Header("token")token: Token,@Body buy: Buy)
+    fun setBarcode(@Header("x-access-token")token: String,@Body barcode: Buy) :Call<Barcode>
 
 
 
--- app/src/main/java/com/example/sunlinhackathon2022/fragment/shop/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/fragment/shop/ProductDetailsActivity.kt
@@ -6,32 +6,61 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.example.sunlinhackathon2022.BarcodeActivity
 import com.example.sunlinhackathon2022.R
+import com.example.sunlinhackathon2022.RetrofitClass
+import com.example.sunlinhackathon2022.Token
 import com.example.sunlinhackathon2022.databinding.ActivityProductDetailsBinding
+import com.example.sunlinhackathon2022.fragment.shop.purchase.Barcode
+import com.example.sunlinhackathon2022.fragment.shop.purchase.Buy
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProductDetailsActivity : AppCompatActivity() {
-    lateinit var  binding:ActivityProductDetailsBinding
+    lateinit var  binding: ActivityProductDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding=ActivityProductDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val sharedPreferences = getSharedPreferences("account", 0)
-        var point=sharedPreferences.getInt("point",0)
+        var point:Int=sharedPreferences.getInt("point",0)
+        var token =sharedPreferences.getString("token","").toString()
+        Log.d("token",token)
+        val intent:Intent= getIntent()
 
-        val price= Integer.getInteger( intent.getStringExtra("price").toString())
+        val price=  intent.getIntExtra("price",-1)
         binding.payButton.setOnClickListener {
-            if(price>point){
-                Log.d("상품 구매","성공")
+            if(true){
+                barcode(token,Buy("test",100,"test"))
             }else{
                 Toast.makeText(this,"포인트가 부족합니다",Toast.LENGTH_SHORT).show()
             }
         }
-
-        val intent:Intent= getIntent()
         binding.productName.text=intent.getStringExtra("name").toString()
         binding.productExplanation.text=intent.getStringExtra("description").toString()
         binding.productTag.text=intent.getStringExtra("tag").toString()
         Glide.with(this).load(intent.getStringExtra("photo")).centerCrop().into(binding.productImageView)
+    }
+    fun barcode(token:String,buy: Buy){
+        val call=RetrofitClass.getApiService().setBarcode(token,buy)
+        call.enqueue(object:Callback<Barcode>{
+            override fun onResponse(call: Call<Barcode>, response: Response<Barcode>) {
+               if(response.isSuccessful){
+                   var intent=Intent(this@ProductDetailsActivity,BarcodeActivity::class.java)
+                   startActivity(intent)
+               }else{
+                   Log.d("adf","adfsaf")
+               }
+            }
+
+            override fun onFailure(call: Call<Barcode>, t: Throwable) {
+                Toast.makeText(this@ProductDetailsActivity,"잠시 후 다시 시도해주세요",Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+
     }
 }
