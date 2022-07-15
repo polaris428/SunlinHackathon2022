afdb3ca07a6ca31b225f36a46e290d06d57f29e5
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-15 19:06:35 +0900

    텍스트 입력시 이동

--- app/src/main/java/com/example/sunlinhackathon2022/fragment/shop/ShopFragment.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/fragment/shop/ShopFragment.kt
@@ -1,6 +1,7 @@
 package com.example.sunlinhackathon2022.fragment.shop
 
 import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Spannable
@@ -13,6 +14,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sunlinhackathon2022.RetrofitClass
+import com.example.sunlinhackathon2022.account.SignInActivity
+import com.example.sunlinhackathon2022.account.SignInData
 import com.example.sunlinhackathon2022.databinding.FragmentShopBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -60,6 +63,10 @@ class ShopFragment : Fragment() {
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
             )
             binding.loginTextView.text=ssb
+            binding.loginTextView.setOnClickListener {
+                var intent=Intent(binding.root.context,SignInActivity::class.java)
+                startActivity(intent)
+            }
         }else{
             binding.loginTextView.visibility=View.GONE
             binding.recyclerView.visibility=View.VISIBLE
