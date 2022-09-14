9855dc88d64517409ddf9cefc55d82b3cc7ee909
iou10 <iou1056212@naver.com>
2022-09-14 10:32:29 +0900

    판다 게임 추가

---
+++ app/src/main/java/com/example/sunlinhackathon2022/minigame/PandaGameActivity.kt
@@ -0,0 +1,36 @@
+package com.example.sunlinhackathon2022.minigame
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.sunlinhackathon2022.AnimalDetailActivity
+import com.example.sunlinhackathon2022.databinding.ActivityPandaGameBinding
+
+class PandaGameActivity : AppCompatActivity() {
+    lateinit var binding:ActivityPandaGameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityPandaGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        click(button = binding.OButton)
+        click(button = binding.XButton)
+
+    }
+    private fun click(button: Button) {
+        button.setOnClickListener {
+            if (button.text == "O") {
+                Toast.makeText(this,"정딥입니다", Toast.LENGTH_SHORT).show()
+                val animalId = intent.getStringExtra("animalCode")
+                val detailIntent = Intent(this, AnimalDetailActivity::class.java)
+                detailIntent.putExtra("animalCode",animalId)
+                startActivity(detailIntent)
+
+            } else {
+                Toast.makeText(this,"다시 한번 시도해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
