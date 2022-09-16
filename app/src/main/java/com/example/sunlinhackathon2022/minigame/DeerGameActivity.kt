9b62ab657a6e3498d715c0d6351b856bf0b09d44
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2022-09-16 10:24:07 +0900

    사슴 미니게임수정

--- app/src/main/java/com/example/sunlinhackathon2022/minigame/DeerGameActivity.kt
+++ app/src/main/java/com/example/sunlinhackathon2022/minigame/DeerGameActivity.kt
@@ -3,21 +3,38 @@ package com.example.sunlinhackathon2022.minigame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.widget.ImageView
 import com.example.sunlinhackathon2022.AnimalDetailActivity
 import com.example.sunlinhackathon2022.R
+import com.example.sunlinhackathon2022.databinding.ActivityDeerGameBinding
+import com.example.sunlinhackathon2022.databinding.ActivityTurtleTouchBinding
 
 class DeerGameActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDeerGameBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_deer_game)
+        binding = ActivityDeerGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var count = 12
         findViewById<ImageView>(R.id.hurtImageView).setOnClickListener {
-            val animalId = intent.getStringExtra("animalCode")
-            val detailIntent = Intent(this, AnimalDetailActivity::class.java)
-            detailIntent.putExtra("animalCode",animalId)
-            startActivity(detailIntent)
-            finish()
+            count--
+            if (count == 0) {
+                binding.examText.visibility = View.GONE
+                binding.answerText.visibility = View.VISIBLE
+                binding.hurtImageView.setImageResource(R.drawable.deer)
+                val handler = Handler()
+                handler.postDelayed(Runnable {
+                    val animalId = intent.getStringExtra("animalCode")
+                    val detailIntent = Intent(this, AnimalDetailActivity::class.java)
+                    detailIntent.putExtra("animalCode", animalId)
+                    startActivity(detailIntent)
+                    finish()
+                }, 3000)
+
+            }
         }
     }
 }
